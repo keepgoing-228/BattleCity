faa0436aacccc678cb5e882810ff3b25dd07b0a8
keepgoing-228 <71744036+keepgoing-228@users.noreply.github.com>
2021-07-14 21:24:18 +0800

    upload Level2

--- src/Background/Grassland.java
+++ src/Background/Grassland.java
@@ -3,14 +3,14 @@ package Background;
 import javax.swing.JLabel;
 
 public class Grassland extends Background {
-
-	public Grassland() {
+	
+	public Grassland () {
 		this.setWalkable(true);
 		this.setPicture(new JLabel(this.getTransformedIcon("static/picture/grassland.png")));
 	}
 	
 	public String toString() {
-		return "This is a piece of grassland";
+		return "This is a grassland";
 	}
 	
 }
